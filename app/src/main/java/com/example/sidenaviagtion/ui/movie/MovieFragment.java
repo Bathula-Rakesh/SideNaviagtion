cffa55e48fcd81808287690c49a9c29a5067436e
Bathula-Rakesh <bathularakesh8222@gmail.com>
2021-10-20 13:17:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sidenaviagtion/ui/movie/MovieFragment.java
@@ -0,0 +1,30 @@
+package com.example.sidenaviagtion.ui.movie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.sidenaviagtion.MainActivity;
+import com.example.sidenaviagtion.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MovieFragment  extends Fragment {
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_movies,container,false);
+
+        return view;
+    }
+}
